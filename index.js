e395ce4284411823c387e9bcbe7b396aa5cdeddb
Situma Bonface Wanga <bonniewanga34@gmail.com>
2023-03-13 20:46:15 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Bonnie Wanga';
+const welcomeMessage = "Welcome to Flatbook, Bonnie Wanga !";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, B !';
